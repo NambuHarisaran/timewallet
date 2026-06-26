328d6872529f6bea6a9e2f462443b4afc8fabefd
NambuHarisaran <nambuharisaran123@gmail.com>
2026-06-26 19:52:41 +0530

    #.0.6 new ui and Bug fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,12 +51,8 @@ android {
             } else {
                 signingConfigs.getByName("debug")
             }
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            ndk {
-                debugSymbolLevel = "none"
-            }
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
